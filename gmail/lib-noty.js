83b72daf395225b7bb38dab6e6af08b490f298f7
Linerre <leon.errelin@outlook.com>
2020-09-15 19:52:44 +0800

    add one more func

--- gmail/lib-noty.js
+++ gmail/lib-noty.js
@@ -19,3 +19,36 @@ function libNotyWatcher() {
  		subjectChecker(libThreads[i], subject, TO_KEEP_SUB, LIB_NOTY.KEEP);
   }
 }
+
+/* libAllSort.gs */
+// take care of messages to lib-all mailing list
+// run every 8 hours
+function libAllSort() {
+	// lib-all invitations --> discard all except workshops
+	// another way to detect invitations: subject:+invitation: has:attachment 
+	var inviteNoWoFilters = `to:${CONTACTS.ALL} label:inbox (invite.ics OR invite.vcs) has:attachment -subject:workshop`;
+	var inviteNoWoThreads = find(inviteNoWoFilters);
+	preClean(LIB_NOTY.CAST, inviteNoWoThreads);
+
+	// to:lib-all from:heads 
+	// loop over heads in inbox, find their threads and process
+  for (const name of Object.keys(HEADS)) {
+    var headFilters = `from:${HEADS[name]} label:inbox`;
+    var headThreads = find(headFilters);
+  // star + mark important + archive, regardless of read/unread
+    collectFromHeads(NYU.HEADS, headThreads);
+  }
+  // select workshops as many as possible
+  // since some are just mentioned rather than a subject
+  // may improve the algorithm later
+  var libWoFilters = `(label:${NYU.HEADS} OR to:${CONTACTS.ALL}) label:inbox subject:workshop`;
+  var libWoThreads = find(libWoFilters);
+  label(NYU.WORK).addToThreads(libWoThreads);
+  // mark unimportant those not from HEADS
+  GmailApp.markThreadsUnimportant(find(`to:${CONTACTS.ALL}) subject:workshop`));
+
+  // handle the rest in the inbox
+  var libRestFilters = `to:${CONTACTS.ALL} OR to:${CONTACTS.COM} label:inbox`;
+  var libRestThreads = find(libRestFilters);
+  preClean(NYU.NYC, libRestThreads);
+}
