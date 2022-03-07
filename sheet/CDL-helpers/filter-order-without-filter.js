be644e14cf37e5d5d18fa6367c3e2e625ab99b78
Noel Errenil <leon.errelin@outlook.com>
2022-03-07 16:11:53 +0800

    Fix HTML errors
    1. Separate table body, header and footer
    2. Use function to pass around an array
    3. Set table style

--- sheet/CDL-helpers/filter-order-without-filter.js
+++ sheet/CDL-helpers/filter-order-without-filter.js
@@ -1,13 +1,20 @@
+// @ts-nocheck
 /**
  * Filter out unnecessary rows and copy the needed to 'Email Susan' Sheet
- * In the future, Email Susan automatically
  * 
  * Author: Errelin
- * Last Change: 2022-03-04
+ * Last Change: 2022-03-07
  */
 
+const ss = SpreadsheetApp.openById(SHIPFIRST.ID)
+const booksSht = ss.getSheetByName('Books');
+const emailSht = ss.getSheetByName('Email Susan');
+let lastTimeLeftAt = emailSht.getLastRow();
+
 function filterCDLs () {
-  let items = booksSht.getRange(5,1,booksSht.getLastRow()-4, booksSht.getLastColumn()).getValues();
+  // Get all the data on Books sheet
+  // [[order1], [order2], ... [orderN]]
+  let items = booksSht.getRange(5,1,booksSht.getLastRow() - 4, booksSht.getLastColumn()).getValues();
   // Logger.log(items)
 
   let nonCDLItems = [];
@@ -23,60 +30,163 @@ function filterCDLs () {
       cdledItems.push(item)
     }
   }
-  // Logger.log('Non CDL Titles: ');
-  // Logger.log(nonCDLItems);
-  checkDup(nonCDLItems,5);
-  checkDup(cdledItems,6);
-  // Logger.log('Yet-to-ship CDL Titles: ');
-  // Logger.log(cdledItems);
+  Logger.log('Successfully filtered orders');
+
+  // Start checking and generating reports 
+  Logger.log('Start constructing table body for non CDL titles ....');
+  let nonCDL = checkDup(nonCDLItems,5);
+  let nonCDLRep = bodyTable(nonCDL, false);
+
+  Logger.log('Start constructing table body for CDL-ed titles ....');
+  let cdled = checkDup(cdledItems,6);
+  let cdledRep = bodyTable(cdled, true);
+
+  draftMaker([nonCDLRep, cdledRep]);
+  Logger.log('Constructing DONE! Draft ready!');
+  
+  Logger.log('Start recording orders on Email Susan sheet ...')
+  recordEmailSusanOrders(nonCDL, cdled);
+  
+  Logger.log('DONE! Check out Susan sheet')
+  let cdlCheckResult = checkCDLIndex(cdled); // get a table about CDL Index
+  return cdlCheckResult;
+
 }
 
 /**
  * Check if the filtered orders have the same item as those of last week 
  */
 function checkDup (items, colNum) {
-  let emailSht = ss.getSheetByName('Email Susan');
-  let lastTimeLeftAt = emailSht.getLastRow();
+  // let emailSht = ss.getSheetByName('Email Susan');
+  // let lastTimeLeftAt = emailSht.getLastRow();
   let existing;
   let emailSusan = [];
 
-  if (colNum == 5) {
-    // Merged cells will be treated as an array with the same width/len as other rows in the range
-    // For example: [Email sent 11/15/2021, , , , ]
-    
-    // Exisiting order numers: array ['NYUSHXXXX', .... , 'NYUSHYYYY']
-    existing = emailSht.getRange(1,1,lastTimeLeftAt,5).getValues().filter(r => r[1].startsWith('NYUSH')).map(r => r[1]);
-
-    elen = existing.length;
-    Logger.log('Already emailed Susan: ');
-    Logger.log(existing);
-  } else {
-    existing = emailSht.getRange(1,6,lastTimeLeftAt,6).getValues().filter(r => r[1].startsWith('NYUSH')).map(r => r[1]);
-    Logger.log('Already emailed Susan: ');
-    Logger.log(existing);
-  }
+  // Merged cells will be treated as an array with the same width/len as other rows in the range
+  // For example: [Email sent 11/15/2021, , , , ]
+  // Exisiting order numers: array ['NYUSHXXXX', .... , 'NYUSHYYYY']
+  existing = colNum == 5 ? 
+  emailSht.getRange(1,1,lastTimeLeftAt,5).getValues().filter(r => r[1].startsWith('NYUSH')).map(r => r[1]) : 
+  emailSht.getRange(1,6,lastTimeLeftAt,6).getValues().filter(r => r[1].startsWith('NYUSH')).map(r => r[1]);
+  
+  Logger.log('Already emailed Susan: ');
+  Logger.log(existing);
 
   // compare Col2 == orderNum
   for (let i = 0, n= items.length; i < n; i++) {
     if(existing.includes(items[i][1])) {
-      Logger.log('Found existing order number %s', items[i][1]);
+      continue;
+      // Logger.log('Found existing order number %s', items[i][1]);
     } else {
+      Logger.log('Found need-to-check order %s, adding it to the list ...', items[i][1]);
       emailSusan.push(items[i]);
     }
   }
 
+  // [[order1], [order2], ... [orderN]]
   if (emailSusan.length > 0) {
     // In this case, drop an draft to YF gmail
     if (colNum == 5) {
       Logger.log('The following Non CDL orders need double checking with Susan');
       Logger.log(emailSusan);
+      // TODO: write emailSusan to Email Susan Sheet 
     } else if (colNum == 6) {
       // Find the row of these titles on CDL sheet as well
       Logger.log('The following CDL-ed orders need double checking with Susan');
       Logger.log(emailSusan);
-    } 
+    }  
   } else {
     // In this case, toast a message? and put a line on the sheet?
     Logger.log('No new orders need double checking with Susan');
   }
+  return emailSusan; 
+}
+
+// Take body text, add headers, footers, make a draft
+function draftMaker (bodyList) {
+  let greetingText = '<p>Hello Susan, </p><p>Could you please help check the following orders? Thank you.</p>';
+  let closingText = '<p>Best regards,<br>Yifei<p>';
+  let emailBody = greetingText + bodyList.join('<br>')  + closingText;
+  GmailApp.createDraft('zl37@nyu.edu', 'Orders to Check', 'Placeholder Text', {from: 'zl37@nyu.edu', htmlBody: emailBody});
+}
+
+function bodyTable (checklist, cdled) {
+  /**
+   * INPUT: [[order1], [order2], [order3], ... [ordern]], true/false
+   * OUTPUT: a string represents a table in HTML 
+   * For non CDL titles
+   * TITLE | ORDER_NUM | CREATE DATE | IPS | NOTE 
+   *  
+   * For CDL-ed titles
+   * TITLE | ORDER_NUM | CREATE DATE | CDL-ed | IPS | NOTE 
+  */
+  if (checklist.length == 0) {
+    Logger.log('There is not item in the checklist.\The body constructor terminates now.')
+    return null;
+  }
+
+  let tableHeader = cdled ? 
+  '<table border="1px" cellpadding="5px" style="border-collapse:collapse;border-color:#666;table-layout:fixed;width:900px"><tbody>\
+  <tr><th>Title</th><th>Order Num</th><th>Order Create Date</th><th>CDL-ed(Y/N/--)</th><th>IPS</th><th>NOTE</th></tr>' : 
+  '<table border="1px" cellpadding="5px" style="border-collapse:collapse;border-color:#666"><tbody>\
+  <tr><th>Title</th><th>Order Num</th><th>Order Create Date</th><th>IPS</th><th>NOTE</th></tr>';  
+  let colNum = cdled ? 6 : 5;
+  
+
+  // empty string
+  let rows = '';
+  if (colNum == 5) {
+    // Non CDL titles
+    for (let i = 0, n = checklist.length; i < n; i++) {
+      if (checklist[i][9].trim() == 'OR') {
+        rows += `<tr><td style="width:450px;word-wrap:break-word">${checklist[i][0]}</td><td>${checklist[i][1]}</td>\
+        <td>${checklist[i][2].toString()}</td><td style="background-color:#FF00FF">${checklist[i][9]}</td><td></td></tr>`;
+      } else {
+        rows += `<tr><td style="width:450px;word-wrap:break-word">${checklist[i][0]}</td><td>${checklist[i][1]}</td>\
+        <td>${checklist[i][2].toString()}</td><td>${checklist[i][9]}</td><td></td></tr>`;
+      }
+    }
+    let emailHeader = '<p><strong>1st Prioritize: Non-CDL titles</strong><p>';
+    let tableFooter = '</tbody></table>';
+    let table = emailHeader + tableHeader + rows + tableFooter;
+    return table;
+  } 
+  
+  if (colNum == 6) {
+    // CDL-ed titles
+    for (let i = 0, n = checklist.length; i < n; i++) {
+      if (checklist[i][9].trim() == 'OR') {
+        rows += `<tr><td style="width:450px;word-wrap:break-word">${checklist[i][0]}</td><td>${checklist[i][1]}</td>\
+        <td>${checklist[i][2].toString()}</td>\
+        <td>${checklist[i][3]}</td><td style="background-color:#FF00FF">${checklist[i][9]}</td><td></td></tr>`;
+      } else {
+         rows += `<tr><td style="width:450px;word-wrap:break-word">${checklist[i][0]}</td><td>${checklist[i][1]}</td>\
+         <td>${checklist[i][2].toString()}</td>\
+         <td>${checklist[i][3]}</td><td>${checklist[i][9]}</td><td></td></tr>`;
+      }
+    }
+    let emailHeader = '<p><strong>2nd Prioritize: CDL titles</strong><p>';
+    let tableFooter = '</tbody></table>';
+    let table = emailHeader + tableHeader + rows + tableFooter;
+    return table;
+  }
+}
+
+function checkCDLIndex (checklist) {
+  let cdlIndexSht = SpreadsheetApp.openById(CDLINDEX.ID).getSheetByName(CDLINDEX.VEND.NAME);
+  let orderNumsIndex= cdlIndexSht.getRange(3,11,cdlIndexSht.getLastRow() - 2, 1).trimWhitespace().getValues().filter(c => c[0].length > 0).map(c => c[0]);
+  let tableHeader = '<table border="1px" cellpadding="5px" style="border-collapse:collapse;border-color:#666;table-layout:fixed;width:900px"><tbody>\
+  <tr><th>Row</th><th>Title</th><th>Order Num</th><th>Order Create Date</th><th>CDL-ed(Y/N/--)</th><th>IPS</th><th>NOTE</th></tr>';
+  let rows = '';
+  let tableFooter = '</tbody></table>';
+
+  for (let i = 0, n = checklist.length; i < n; i++) {
+    if (orderNumsIndex.includes(checklist[i][1].trim())) {
+      Logger.log('Found this title on CDL Index at row %d', orderNumsIndex.indexOf(checklist[i][1]) + 3);
+      rows += `<tr><td>${orderNumsIndex.indexOf(checklist[i][1]) + 3}</td><td style="width:450px;word-wrap:break-word">${checklist[i][0]}</td>\
+      <td>${checklist[i][1]}</td><td>${checklist[i][2]}</td><td>${checklist[i][3]}</td>\
+      <td style="background-color:yellow">${checklist[i][9]}</td><td></td></tr>`;
+    }
+  }
+  return tableHeader + rows + tableFooter;
 }
