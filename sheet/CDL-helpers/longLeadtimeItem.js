3b1a4d967497f98a8d9d4c7da888d1c77ca671bf
Noel Errenil <leon.errelin@outlook.com>
2022-03-09 12:54:38 +0800

    Refactor checking algorithm
    1. Removed unnecessary checking for physical copies
    2. Improve checking criteria to day level to avoid fake positive:
    for example, the days between 2022-2-15 and 2022-3-8 are not as many as that of a normal month
    3. Make email sender and receiver easier for testing

--- sheet/CDL-helpers/longLeadtimeItem.js
+++ sheet/CDL-helpers/longLeadtimeItem.js
@@ -4,13 +4,18 @@
  * 2. scanned but the physical copies are not back to KARMS within 1 month
  * 
  * Author: Errelin
- * Last Change: 2022-03-08
+ * Last Change: 2022-03-09
  */
 
 const CDLINDEX = {
   ID    : '1DpgC__qjQmxrY1Mltm2OKl-tDn-1w4pFsSiKXZPV1ao',
   URL   : 'https://docs.google.com/spreadsheets/d/1DpgC__qjQmxrY1Mltm2OKl-tDn-1w4pFsSiKXZPV1ao/edit?usp=sharing'
 }
+
+const USERS = {
+  ME: 'zl37@nyu.edu',
+  YF: 'yz8212@nyu.edu'
+};
 
 function longLeadtimeItems() {
   let range = vendorCDLSheet.getRange(129, 1, vendorCDLSheet.getLastRow(), vendorCDLSheet.getLastColumn());
@@ -23,7 +28,8 @@ function longLeadtimeItems() {
   let today = new Date();
   let inYear = today.getFullYear();
   let inMonth = today.getMonth() + 1;
-  // let ondDay = today.getDate();
+  let ondDay = today.getDate();
+  
   let trackingNotes = [];
   let vendorToScan = [];
   let waitingForPrint = [];
@@ -33,94 +39,79 @@ function longLeadtimeItems() {
     // Skip cancelled or physically arrived titles
     if (item[1] == 'Cancelled' || item[0] == 'CDL Silent') {
       continue;
-    } 
+    }
 
-    // Requestd 
-    if (item[12] !== '') {
-      let req = item[12];
-      let reqYear = req.getFullYear();
-      let reqMonth = req.getMonth() + 1;
-      // let reqDay = req.getDate();
-      
-      // Yet to send to scan
-      if (item[13] === '') {
-        // Has tracking note?
-        if (item[8] !== '') {
-          // Email YF or not?
-          Logger.log('Still waiting for vendor PDF at row %d. \nThe note is %s', i+129, item[8]);
-          let tracked = {
-            ORDERD    : item[11].toLocaleDateString(),
-            TITLE     : item[5],
-            ORDER_NUM : item[10],
-            BARCODE   : item[9],
-            TRACKINGN : item[8],
-            LIBNOTES  : item[7]
-          };
-          trackingNotes.push(tracked);
-        } else {// Not scanned and no tracking note         
-          // Ordered last year
-          if (inYear - reqYear == 1) {
-            // and longer than 1 month
-            if (12 - reqMonth + inMonth > 1) {
+    /**
+     * | Tracking Notes | ... | Order Date |	Scanning Vendor Payment Date |	PDF Delivery Date |
+     * |     I 9        | ... |    M 13    |              N 14             |        O 15        |
+     */ 
+
+    // let req = item[12];
+    // let reqYear = req.getFullYear();
+    // let reqMonth = req.getMonth() + 1;
+    // let reqDay = req.getDate();
+    
+    // Yet to send to scan
+    if (item[13] === '') {// Not paid vendor yet (N)
+      if (item[8] !== '') {// Has tracking note
+        // Email YF or not?
+        Logger.log('Have not paid vendor yet, at row %d. \nThe note is %s', i+129, item[8]);
+        let tracked = {
+          ORDERD    : item[11].toLocaleDateString(),
+          TITLE     : item[5],
+          ORDER_NUM : item[10],
+          BARCODE   : item[9],
+          TRACKINGN : item[8],
+          LIBNOTES  : item[7]
+        };
+        trackingNotes.push(tracked);  
+      } 
+    } else {// Paid vendor: item[13] !== ''
+      if (item[14] == '' && item[8] == '') {// but has NOT got PDF and has NO tracking note
+        let paid = item[13]
+        let paidYear = paid.getFullYear();
+        let paidMonth = paid.getMonth() + 1;
+        let paidDay = paid.getDate();
+
+        if (inYear - paidYear >= 1) {// Paid in the past year(s)
+          if (12 - paidMonth + inMonth > 1) {// and waiting for more than 2 month
+            Logger.log('Waiting for PDF for over 1 month at row %d', i+129);
+            let toscan = {
+              ORDERD    : item[11].toLocaleDateString(),
+              TITLE     : item[5],
+              ORDER_NUM : item[10],
+              BARCODE   : item[9],
+              TRACKINGN : item[8],
+              LIBNOTES  : item[7]
+            };
+            vendorToScan.push(toscan);
+          } else if ((12 - paidMonth + inMonth == 1) && (31 - paidDay + ondDay > 30)) {// waiting for more than 1 month but not as long as 2
               Logger.log('Waiting for PDF for over 1 month at row %d', i+129);
               let toscan = {
                 ORDERD    : item[11].toLocaleDateString(),
                 TITLE     : item[5],
                 ORDER_NUM : item[10],
                 BARCODE   : item[9],
+                TRACKINGN : item[8],
+                LIBNOTES  : item[7]
               };
               vendorToScan.push(toscan);
-            }
-          } else if (inYear - reqYear == 0) { // Ordered in the same year
-            if (inMonth - reqMonth > 1) {
+          }
+        } else if (inYear - paidYear == 0) { // Paid in the same year
+            if ((inMonth - paidMonth > 1) && (31 - paidDay + ondDay > 30)) {
               Logger.log('Waiting for PDF over 1 month at row %d', i+129);
               let toscan = {
                 ORDERD    : item[11].toLocaleDateString(),
                 TITLE     : item[5],
                 ORDER_NUM : item[10],
                 BARCODE   : item[9],
+                TRACKINGN : item[8],
+                LIBNOTES  : item[7]
               };
               vendorToScan.push(toscan);
             }
-          } else if (inYear - reqYear > 1) { // Waiting for over one year
-              Logger.log('Waiting for scaning for over ONE year!');
-          }
-        }
-      }
-
-      // Sent to scan (N !== '')
-      if (item[13] !== '') {
-        let req = item[13];
-        let reqYear = req.getFullYear();
-        let reqMonth = req.getMonth() + 1;
-        // let reqDay = req.getDate();
-        // Physical copy not arrived
-        if (item[15] == '') {
-          if (inYear - reqYear == 1) {
-            if (12 - reqMonth + inMonth > 1) {
-              Logger.log('Waiting for print for over 1 month at row %d', i+129)
-              let printToArrive = {
-                ORDERD    : item[11].toLocaleDateString(),
-                TITLE     : item[5],
-                ORDER_NUM : item[10],
-                BARCODE   : item[9],
-              };
-              waitingForPrint.push(printToArrive);
-            } else if (inYear - reqYear == 0) { // Ordered in the same year
-              if (inMonth - reqMonth > 1) {
-                Logger.log('Waiting for print for over 1 month at row %d', i+129);
-                let printToArrive = {
-                  ORDERD    : item[11].toLocaleDateString(),
-                  TITLE     : item[5],
-                  ORDER_NUM : item[10],
-                  BARCODE   : item[9],
-                };
-                waitingForPrint.push(printToArrive);
-              } else if (inYear - reqYear > 1) { // Waiting for over one year
-                Logger.log('Waiting for print for over ONE year!');   
-              } 
-            }
-          }
+        } else if (inYear - paidYear > 1) { // Waiting for over one year
+            Logger.log('Waiting for scaning for over ONE year!');
         }
       }
     }
@@ -129,7 +120,7 @@ function longLeadtimeItems() {
   Logger.log(trackingNotes);
   let header = `<p> A CDL order will be listed in this email if:\
   <ol>\
-  <li>Scanning Vendor Payment Date remians empty for at least one month or</li>\
+  <li>Scanning Vendor Payment Date remians empty for at least one month <em>or</em></li>\
   <li>Vendor got paid but PDF Delivery Date remians empty for at least one month</li>\
   </ol>\
   <p>The following CDL orders on <a href="${CDLINDEX.URL}">CDL Index</a> need your attention:</p>`;
@@ -143,10 +134,11 @@ function longLeadtimeItems() {
 
   let body = header + withTrackingNotes + yetToScan + separator + footer;
 
-  GmailApp.sendEmail('zl37@nyu.edu', 'Long Waiting CDL Orders', 'Placeholder text', {
-    from: 'zl37@nyu.edu',
-    name: 'Long-Waiting-CDL', 
-    cc: 'zl37@nyu.edu',
+  GmailApp.sendEmail(USERS.YF, 'Long Waiting CDL Orders', 'Placeholder text', {
+    from: USERS.ME,
+    name: 'Long-Waiting-CDL',
+    // cc: USERS.ME,
+    cc: USERS.ME + ',' + USERS.YF,
     htmlBody: body,
     noReply: true});
 
@@ -163,7 +155,7 @@ function tableConstruct(checklist, checkType) {
   }
   let tableHeader = '<table border="1px" cellpadding="5px" style="border-collapse:collapse;border-color:#666;table-layout:fixed;width:1000px"><tbody>\
   <tr><th style="width:120px">Order Date</th>\
-  <th style="width:120px">Order Num</th>\
+  <th style="width:120px">Order Number</th>\
   <th style="width:340px;word-wrap:break-word">Title</th>\
   <th style="width:210px">Library Notes</th>\
   <th style="width:210px">Tracking Notes</th></tr>';
