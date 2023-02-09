88b87e94b2a5d41405abc2f54d738a969569b973
Noel Errenil <leon.errelin@outlook.com>
2023-02-09 16:32:45 +0800

    Update columns
    The order of columns has been adjusted.
    Plus, there is one more row on the top.

--- sheet/CDL-helpers/constants.js
+++ sheet/CDL-helpers/constants.js
@@ -28,42 +28,41 @@ var activeSheetName = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().get
 var lastRowVendor = vendorCDLSheet.getLastRow();
 // var lastRowLocal = localCDLSheet.getLastRow();
 
-// Get J=10 column of titles
+// Get F=7 column of titles
 // Note that here lastRow is safe since empty title means empty row
-var vendorTitles = vendorCDLSheet.getRange(2, 10, lastRowVendor - 1).getValues().filter(t => t[0].length > 0);
-// var localTitles = localCDLSheet.getRange(2, 10, lastRowLocal - 1).getValues().filter(t => t[0].length > 0);
+var vendorTitles = vendorCDLSheet.getRange(3, 7, lastRowVendor - 1).getValues().filter(t => t[0].length > 0);
+// var localTitles = localCDLSheet.getRange(3, 7, lastRowLocal - 1).getValues().filter(t => t[0].length > 0);
 
 // this is the real last row, the row that holds a title
 var realVendorLastRow = vendorTitles.length;
-var realLocalLastRow = localTitles.length;
+// var realLocalLastRow = localTitles.length;
 
 // get E=5 column of available dates
-var vendorAvailableDates = vendorCDLSheet.getRange(2, 5, realVendorLastRow).getValues();
-// var localAvailableDates = localCDLSheet.getRange(2, 5, realLocalLastRow).getValues();
+var vendorAvailableDates = vendorCDLSheet.getRange(3, 5, realVendorLastRow).getValues();
+// var localAvailableDates = localCDLSheet.getRange(3, 5, realLocalLastRow).getValues();
 
-// Get I=9 column of barcodes
+// Get J=10 column of barcodes
 // note that barcodes with strikthrough will be counted as well
 // filtering empty cells risk losing those that are going to be filled in
 // instead, set the lastRow var wisely and dynamically accoridng to num of titles
-var vendorBarcodeColHeader = vendorCDLSheet.getRange(1, 9).getValue();
-var vendorBarcodes = vendorCDLSheet.getRange(2, 9, realVendorLastRow).getValues().map(b => b[0].toString());
-// var localBarcodes = localCDLSheet.getRange(2, 9, realLocalLastRow).getValues().map(b => b[0].toString());
+var vendorBarcodeColHeader = vendorCDLSheet.getRange(2, 10).getValue();
+var vendorBarcodes = vendorCDLSheet.getRange(3, 10, realVendorLastRow).getValues().map(b => b[0].toString());
+// var localBarcodes = localCDLSheet.getRange(3, 10, realLocalLastRow).getValues().map(b => b[0].toString());
 
 // Get J=10 column of bobcat links
-var vendorBobcatUrl = vendorCDLSheet.getRange(2, 13, realVendorLastRow).getValues().map(b => b[0]);
-// var localBarcodes = localCDLSheet.getRange(2, 13, realLocalLastRow).getValues().map(b => b[0]);
-
+var vendorBobcatUrl = vendorCDLSheet.getRange(3, 13, realVendorLastRow).getValues().map(b => b[0]);
+// var localBarcodes = localCDLSheet.getRange(3, 13, realLocalLastRow).getValues().map(b => b[0]);
 // Get K=11 column of vendor links
 // not applicable to local CDL
-var vendorFileUrl = vendorCDLSheet.getRange(2, 11, realVendorLastRow).getValues().map(b => b[0]);
+var vendorFileUrl = vendorCDLSheet.getRange(3, 11, realVendorLastRow).getValues().map(b => b[0]);
 
-// Get O=15 column of circ copy url
+// Get E=5 column of circ copy url
 var circUrlVendorSheet = vendorCDLSheet
-                        .getRange(2, 15, realVendorLastRow)
+                        .getRange(3, 5, realVendorLastRow)
                         .getValues()
                         .map(u => u[0]);
 // var circUrlLocalSheet = localCDLSheet
-//                         .getRange(2, 15, realLocalLastRow)
+//                         .getRange(3, 5, realLocalLastRow)
 //                         .getValues()
 //                         .map(u => u[0]);
 
